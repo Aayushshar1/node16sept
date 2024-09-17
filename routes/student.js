2912ad43ee42a5edf0e4bda6d368575c96115eb9
aayush sharawat <aayushsha9814@gmail.com>
2024-09-17 14:41:15 +0530

    updation

---
+++ routes/student.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const studentController = require('../controllers/studentcontroller');
+const router = express.Router();
+
+router.post('/add/user',(req,res)=>{
+    studentController.addStudent(req,res);
+      
+})
+router.get('/users',(req,res)=>{
+    studentController.getStudents(req,res);
+
+})
+
+module.exports = router;
